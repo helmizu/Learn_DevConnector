8c5066295db72d369bb00985fa77f58a12237ee5
HelmiZu <helmi_suprayitno_25rpl@student.smktelkom-mlg.sch.id>
2018-07-19 16:56:34 +0700

    login and register user

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authActions'
+import classnames from 'classnames'
 
-export default class Login extends Component {
+class Login extends Component {
     constructor(){
         super()
         this.state = {
@@ -12,6 +16,22 @@ export default class Login extends Component {
         this.onSubmit = this.onSubmit.bind(this)
     }
     
+    componentDidMount(){
+        if(this.props.auth.isAuthenticated) {
+            this.props.history.push('/dashboard')
+        }
+    }
+    
+    componentWillReceiveProps(nextProps){
+        if(nextProps.auth.isAuthenticated) {
+            this.props.history.push('/dashboard')
+        }
+
+        if(nextProps.errors) {
+            this.setState({errors: nextProps.errors})
+        }
+    }
+
     onChange(e) {
         this.setState({
             [e.target.name] : e.target.value
@@ -22,13 +42,15 @@ export default class Login extends Component {
         e.preventDefault()
         
         const loginUser = {
-            email: this.state.email,
+            email: this.state.email.toLowerCase(),
             password: this.state.password,
         }
         
-        console.log(loginUser)
+        this.props.loginUser(loginUser)
     }
     render() {
+        const { errors } = this.state
+
         return (
             <div className="login">
             <div className="container">
@@ -38,10 +60,20 @@ export default class Login extends Component {
             <p className="lead text-center">Sign in to your DevConnector account</p>
             <form onSubmit={this.onSubmit}>
             <div className="form-group">
-            <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
+            <input type="email" className={classnames("form-control form-control-lg", { 'is-invalid': errors.email } )} placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
+            {errors.email && (
+                <div className="invalid-feedback">
+                    {errors.email}
+                </div>
+            )}
             </div>
             <div className="form-group">
-            <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} />
+            <input type="password" className={classnames("form-control form-control-lg", { 'is-invalid': errors.password } )} placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} />
+            {errors.password && (
+                <div className="invalid-feedback">
+                    {errors.password}
+                </div>
+            )}
             </div>
             <input type="submit" className="btn btn-info btn-block mt-4" />
             </form>
@@ -52,3 +84,16 @@ export default class Login extends Component {
         )
     }
 }
+
+Login.propTypes = {
+    loginUser : PropTypes.func.isRequired,
+    auth : PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    auth : state.auth,
+    errors : state.errors
+})
+
+export default connect(mapStateToProps, { loginUser })(Login)
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types'
-import axios from 'axios'
 import classnames from 'classnames'
 import { connect } from 'react-redux'
 import { registerUser } from '../../actions/authActions'
@@ -19,6 +19,18 @@ class Register extends Component {
         this.onChange = this.onChange.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
     }
+    
+    componentDidMount(){
+        if(this.props.auth.isAuthenticated) {
+            this.props.history.push('/dashboard')
+        }
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.errors) {
+            this.setState({ errors: nextProps.errors })
+        }
+    }
 
     onChange(e) {
         this.setState({
@@ -30,26 +42,20 @@ class Register extends Component {
         e.preventDefault()
 
         const newUser = {
-            name: this.state.name,
-            email: this.state.email,
+            name: this.state.name.toLowerCase(),
+            email: this.state.email.toLowerCase(),
             password: this.state.password,
             password2: this.state.password2
         }
 
-        this.props.registerUser(newUser)
-        //axios.post('/api/users/register', newUser)
-        //.then(res => console.log(res.data))
-        //.catch(err => this.setState({errors : err.response.data}))
-        //console.log(newUser)
+        this.props.registerUser(newUser, this.props.history)
     }
 
     render() {
         const { errors } = this.state
-        const { user } = this.props.auth
 
         return (
             <div className="register">
-            { user ? user.name : null}
             <div className="container">
             <div className="row">
             <div className="col-md-8 m-auto">
@@ -101,11 +107,13 @@ class Register extends Component {
 
 Register.propTypes = {
     registerUser : PropTypes.func.isRequired,
-    auth : PropTypes.object.isRequired
+    auth : PropTypes.object.isRequired,
+    errors : PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state) => ({
-    auth: state.auth
+    auth: state.auth,
+    errors: state.errors
 })
 
-export default connect(mapStateToProps, { registerUser })(Register)
+export default connect(mapStateToProps, { registerUser })(withRouter(Register))
